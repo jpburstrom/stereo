77304f33cf606f617d381322f6c916d89528b323
Anton Lindqvist <anton@qvister.se>
2010-07-29 20:31:34 +0200

    Moved partitioned resource class into a separate file.

--- soundcloud.php
+++ soundcloud.php
@@ -6,55 +6,6 @@
  * @version 1.1
  * @link http://github.com/mptre/php-soundcloud/
  */
- // Extension of Array to allow linked partitioning
- class PartitionedResource extends ArrayObject
- {
-   function __construct($string) {
-       $data = $this->parse_to_array($string);
-       parent::__construct($data);
-   }
-
-    public function get_next_partition($soundcloud) {
-      $next_partition_url =  $this['@attributes']['next-partition-href'];
-
-      if ($next_partition_url != '')
-      {
-        preg_match("/([a-z]+)\?/", $next_partition_url, $matches);
-        $method = $matches[0];
-
-        preg_match("/(\?)(.+)/", $next_partition_url, $matches);
-        $param = $matches[2];
-
-        $string = $soundcloud->request($method.$param);
-        return new PartitionedResource($string);
-      }
-      else {
-        return '';
-      }
-    }
-
-    /// Turns the string that the main PHP API returns into an array that works with our resource.
-    private function parse_to_array($string) {
-      // SimpleXMLElement fails due to some of the characters in the query string, so they get stripped out here...
-      if (strstr($string, 'next-partition-href'))
-      {
-        preg_match("/\/([a-z]+\?.*)\"/", $string, $matches);
-        $queryparams = $matches[1];
-        $string = str_replace($queryparams, "", $string);
-      }
-
-      $data = new SimpleXMLElement($string);
-      $data = get_object_vars($data);
-      // ...and replaced here.
-      if ($data['@attributes']['next-partition-href'])
-      {
-        $data['@attributes']['next-partition-href'] = $data['@attributes']['next-partition-href'].$queryparams;
-      }
-      return $data;
-    }
-}
-
-
 class Soundcloud {
 
     const VERSION = '1.1.0';
