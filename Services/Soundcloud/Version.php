b817fef4cbdf69f48554e8ddc73fc286a7237499
Anton Lindqvist <anton@qvister.se>
2011-01-23 21:00:36 +0100

    Bumped version.

--- Services/Soundcloud/Version.php
+++ Services/Soundcloud/Version.php
@@ -13,7 +13,7 @@ class Services_Soundcloud_Version {
 
     const MAJOR = 2;
     const MINOR = 1;
-    const PATCH = 0;
+    const PATCH = 1;
 
     public static function get() {
         return implode('.', array(self::MAJOR, self::MINOR, self::PATCH));
