8638f76f394b9fc181db0d5605401c9f281dc26c
Johannes Burström <johannes@ljud.org>
2013-05-20 07:47:47 +0200

    require p2p +scb

--- stereo.php
+++ stereo.php
@@ -5,12 +5,37 @@
  * Author: Johannes Burström
  */
 
-include("admin/stereo-options.php");
+require dirname( __FILE__ ) . "/scb/load.php";
+
+scb_init('stereo_init');
+
+function stereo_init() {
+	add_action( 'plugins_loaded', 'stereo_load_p2p_core', 20 );
+	add_action( 'init', 'stereo_connection_types' );
+}
+
+function stereo_load_p2p_core() {
+	if ( function_exists( 'p2p_register_connection_type' ) )
+		return;
+
+	define( 'P2P_TEXTDOMAIN', 'stereo' );
+
+	require_once dirname( __FILE__ ) . '/p2p-core/init.php';
+
+	// TODO: can't use activation hook
+	add_action( 'admin_init', array( 'P2P_Storage', 'install' ) );
+}
+
+function stereo_connection_types() {
+	p2p_register_connection_type( array(
+		'name' => 'playlist_to_tracks',
+		'from' => 'stereo_playlist',
+		'to' => 'stereo_track',
+        'cardinality' => 'one-to-many'
+	) );
+}
+
+require("admin/stereo-options.php");
 //ID3, save & edit attachment metadata
-include("admin/stereo-attachments.php");
-include("admin/stereo-custom-post.php");
-
-
-
-
-
+require("admin/stereo-attachments.php");
+require("admin/stereo-custom-post.php");
