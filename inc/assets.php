eba8c7056d77b06b9b394c961213c75584d2f002
Johannes Burström <johannes@ljud.org>
2013-10-20 14:02:41 +0200

    Whitespace

--- inc/assets.php
+++ inc/assets.php
@@ -10,7 +10,9 @@ function stereo_enqueue_assets() {
     $cssdir = STEREO_PLUGIN_URL . "css/";
     $jsdir = STEREO_PLUGIN_URL . "js/";
     wp_register_style('stereo-widget', "$cssdir/stereo-widget.css");
+
     wp_register_script('stereo-widget', "$jsdir/stereo-widget.js", array("jquery")); #XXX
+
 }
 
 add_action( 'wp_enqueue_scripts', 'stereo_enqueue_assets' );
