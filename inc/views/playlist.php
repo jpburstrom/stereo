2dc81087c101df72284a3757f1a9e9d4f93d130f
Johannes Burström <johannes@ljud.org>
2014-04-23 06:33:18 +0200

    Add 'active' class for non-empty tracks

--- inc/views/playlist.php
+++ inc/views/playlist.php
@@ -1,7 +1,7 @@
 <ul class="playlist stereo" data-stereo-playlist="<?php echo $post->ID?>">
 <?php do_action('stereo_playlist_pre', $post) ?>
 <?php while ( $connected->have_posts() ) : $connected->the_post(); $meta = get_stereo_track_meta($post->ID); ?>
-    <li class="stereo track" data-stereo-track="<?php echo $post->ID?>">
+    <?php echo the_stereo_track_tag($meta, "li"); ?>
         <?php do_action('stereo_playlist_track_pre', $post, $meta) ?>
         <span class="stereo-track-number"><?php echo apply_filters("stereo_playlist_track_number", $post->menu_order, $post, $meta) ?></span>
         <span class="stereo-track-title"><?php echo apply_filters("stereo_playlist_track_title", get_the_title(), $post, $meta) ?></span>
